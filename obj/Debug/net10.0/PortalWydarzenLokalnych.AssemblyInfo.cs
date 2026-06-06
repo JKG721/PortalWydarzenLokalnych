d39d675d15bd88112246b49daabb2599f094b156
Kuba Gorczyca <kubagorczyca100@gmail.com>
2026-06-06 02:08:35 +0200

    Poprawka nawigacji

--- obj/Debug/net10.0/PortalWydarzenLokalnych.AssemblyInfo.cs
+++ obj/Debug/net10.0/PortalWydarzenLokalnych.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PortalWydarzenLokalnych")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3f31ce6ded6a798068366090abda1f54b856def")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e46789d2d12412f561099b42552d89c177d94c1c")]
 [assembly: System.Reflection.AssemblyProductAttribute("PortalWydarzenLokalnych")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PortalWydarzenLokalnych")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
